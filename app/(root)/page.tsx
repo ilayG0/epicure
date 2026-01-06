d7d791a33a222dc88312ab0a38e5c69425132218
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2026-01-06 17:53:44 +0200

    about section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import { Dish } from "@/types/dish.type";
 import { Icon } from "@/types/icon.type";
 import SignatureDishes from "@/components/home/signature-dishes-section";
 import IconsSection from "@/components/home/icons-section";
+import AboutEpicure from "@/components/home/about-section";
 
 // move to a help function service
 async function getRestaurants(): Promise<Restaurant[]> {
@@ -28,16 +29,25 @@ async function getIcons(): Promise<Icon[]> {
   return res.json();
 }
 
+async function getAbout(): Promise<string> {
+  const res = await fetch("http://localhost:3000/api/about", {
+    cache: "no-store",
+  });
+  return res.json();
+}
+
 const Page = async () => {
   const restaurants: Restaurant[] = await getRestaurants();
   const dishes: Dish[] = await getSignatureDishes();
   const icons: Icon[] = await getIcons();
+  const about: string = await getAbout();
   return (
     <>
       <Hero />
       <PopularRestaurants restaurants={restaurants} />
       <SignatureDishes dishes={dishes} />
       <IconsSection icons={icons} />
+      <AboutEpicure about={about}/>
     </>
   );
 };
